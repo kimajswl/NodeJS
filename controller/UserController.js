c5e887eb008ffcb7b76a946152bcf1bb94033d05
Kimajswl <ajswl4763@naver.com>
2024-02-10 12:21:44 +0900

    CRUD 완성

--- controller/UserController.js
+++ controller/UserController.js
@@ -12,7 +12,38 @@ const addUser = async (req, res) => {
     res.status(200).send(user);
 };
 
+const getAllUsers = async (req, res) => {
+    const users = await User.findAll({}).catch((err) => console.log(err));
+    res.status(200).send(users);
+};
+
+const getUser = async (req, res) => {
+    let uid = req.params.id;
+    let user = await User.findOne({ where: { id: uid } }).catch((err) =>
+        console.log(err)
+    );
+    res.status(200).send(user);
+}
+
+const updateUser = async (req, res) => {
+    let id = req.params.id;
+    const user = await User.update(req.body, { where: { id: id } }).catch((err) =>
+        console.log(err)
+    );
+    res.status(200).send(user);
+};
+
+const deleteUser = async (req, res) => {
+    let id = req.params.id;
+    await User.destroy({ where: { id: id } }).catch((err) => console.log(err));
+    res.status(200).send("User is deleted");
+};
+
 module.exports = {
-    addUser
+    addUser,
+    getUser,
+    getAllUsers,
+    updateUser,
+    deleteUser
 };
 
--- routers/UserRouter.js
+++ routers/UserRouter.js
@@ -3,5 +3,9 @@ const userController = require("../controller/UserController.js");
 const router = require("express").Router();
 
 router.post("/addUser", userController.addUser);
+router.get("/getAllUsers", userController.getAllUsers);
+router.get("/getUser/:id", userController.getUser);
+router.put("/updateUser/:id", userController.updateUser);
+router.delete("/deleteUser/:id", userController.deleteUser);
 
 module.exports = router;
